b97bf603d23a64dbb2ec8b2d8e3080410ecf224d
Alexeyalexeyalex <34206586+Alexeyalexeyalex@users.noreply.github.com>
2019-02-04 23:40:09 +0300

    Окно заставки

---
+++ Приложение/PO/PO/Zastavka.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace PO
+{
+    partial class Zastavka
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Zastavka));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.metroProgressBar1 = new MetroFramework.Controls.MetroProgressBar();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 400;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // metroProgressBar1
+            // 
+            this.metroProgressBar1.Location = new System.Drawing.Point(23, 117);
+            this.metroProgressBar1.Name = "metroProgressBar1";
+            this.metroProgressBar1.Size = new System.Drawing.Size(533, 33);
+            this.metroProgressBar1.TabIndex = 4;
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(553, 208);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(93, 49);
+            this.metroPanel1.TabIndex = 5;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            this.metroPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.metroPanel1_Paint);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel1.Location = new System.Drawing.Point(439, 185);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(108, 25);
+            this.metroLabel1.TabIndex = 5;
+            this.metroLabel1.Text = "Загрузка 0%";
+            // 
+            // Zastavka
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 230);
+            this.Controls.Add(this.metroPanel1);
+            this.Controls.Add(this.metroProgressBar1);
+            this.Controls.Add(this.metroLabel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Zastavka";
+            this.Text = "Загрузка";
+            this.Load += new System.EventHandler(this.Zastavka_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBar1;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+    }
+}
---
+++ Приложение/PO/PO/Zastavka.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PO
+{
+    public partial class Zastavka : MetroFramework.Forms.MetroForm
+    {
+        
+        public Zastavka()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        Task ProcessImport(List<string> data, IProgress<ProgressReport> progress)
+        {
+            int index = 1;
+            int totalProgress = data.Count;
+            var progressReport = new ProgressReport();
+            return Task.Run(() =>
+            {
+                for(int i=0; i< totalProgress; i++)
+                {
+                    progressReport.PercentComplete = index++ * 100 / totalProgress;
+                    progress.Report(progressReport);
+                    Thread.Sleep(6);
+                }
+            });
+        }
+
+
+        private async void Zastavka_Load(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>();
+            for (int i = 0; i < 1000; i++)
+                list.Add(i.ToString());
+            metroLabel1.Text = "Working...";
+            var progressReport = new Progress<ProgressReport>();
+            progressReport.ProgressChanged += (o, report) =>
+            {
+                metroLabel1.Text = string.Format("Загрузка...{0}%", report.PercentComplete);
+                metroProgressBar1.Value = report.PercentComplete;
+                metroProgressBar1.Update();
+            };
+            await ProcessImport(list, progressReport);
+            Glavn glavn = new Glavn();
+            glavn.Show();
+            this.Hide();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Random rand = new Random();
+
+            //if (metroProgressSpinner1.Value == 100)
+            //{
+            //    Glavn glavn = new Glavn();
+            //    glavn.Show();
+            //    this.Hide();
+            //    timer1.Enabled = false;
+            //}
+            //else
+            //{
+            //    metroProgressSpinner1.Step = rand.Next(10, 16);
+            //    metroProgressSpinner1.PerformStep();
+            //}
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void metroPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private async void metroButton1_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+    }
+
+
+}
